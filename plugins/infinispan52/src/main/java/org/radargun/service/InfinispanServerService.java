e96db2d578867b5c834527974d356a6ad9a1a09f
jmarkos <jmarkos@redhat.com>
2016-09-12 17:21:05 +0200

    Add serverZip property to prepare the server from zip automatically

--- plugins/infinispan52/src/main/java/org/radargun/service/InfinispanServerService.java
+++ plugins/infinispan52/src/main/java/org/radargun/service/InfinispanServerService.java
@@ -8,16 +8,22 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import javax.management.MBeanServerConnection;
 import javax.management.remote.JMXConnector;
 
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
 import org.radargun.Service;
 import org.radargun.ServiceHelper;
 import org.radargun.config.Destroy;
@@ -44,6 +50,9 @@ public class InfinispanServerService extends JavaProcessService {
 
    @Property(doc = "Home directory for the server")
    private String home;
+
+   @Property(doc = "Server zip. If set, the zip will be extracted to the 'home' directory.")
+   private String serverZip;
 
    @Property(doc = "Java binary used to start the server. Default is system-default.")
    private String java;
@@ -89,6 +98,25 @@ public class InfinispanServerService extends JavaProcessService {
       clustered = new InfinispanServerClustered(this);
 
       try {
+         if (serverZip != null) {
+            Path homePath = FileSystems.getDefault().getPath(home);
+            Utils.deleteDirectory(homePath.toFile());
+            ZipFile zipFile = new ZipFile(serverZip);
+            zipFile.extractAll(homePath.toString());
+            List<Path> homeContents = Files.list(homePath).collect(Collectors.toList());
+            // move the server files 1 directory up (remove the extra directory)
+            if (homeContents.size() == 1) {
+               Path fromDir = homeContents.get(0);
+               for (Path path : Files.list(fromDir).collect(Collectors.toList())) {
+                  Files.move(path, FileSystems.getDefault().getPath(home, path.getFileName().toString()));
+               }
+               Files.delete(fromDir);
+            }
+            // the extraction erases the executable bits
+            Path scriptPath = FileSystems.getDefault().getPath(home, "bin", getStartScriptPrefix() + (windows ? "bat" : "sh"));
+            Set<PosixFilePermission> permissions = PosixFilePermissions.fromString("rwxr-xr-x");
+            Files.setPosixFilePermissions(scriptPath, permissions);
+         }
          URL resource = getClass().getResource("/" + file);
          Path filesystemFile = FileSystems.getDefault().getPath(file);
          Path target = FileSystems.getDefault().getPath(home, "standalone", "configuration", "radargun-" + ServiceHelper.getSlaveIndex() + ".xml");
@@ -111,8 +139,10 @@ public class InfinispanServerService extends JavaProcessService {
             throw new FileNotFoundException("File " + file + " not found neither as resource not in filesystem.");
          }
       } catch (IOException e) {
-         log.error("Failed to copy configuration file", e);
+         log.error("Failed to prepare the server directory", e);
          throw new RuntimeException(e);
+      } catch (ZipException e) {
+         log.error("Failed to extract the server zip", e);
       }
       lifecycle.addListener(new ProcessLifecycle.ListenerAdapter() {
          private JMXConnector connector;
